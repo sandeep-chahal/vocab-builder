c11aad22e38c48b6135f7df06ed5202fca03772b
sandeep chahal <Sandeepchahal573@gmail.com>
2021-01-15 16:04:59 +0530

    craeted simple navbar

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,9 +1,17 @@
 import styles from "./styles.module.css";
+import Link from "next/link";
 
 const Layout = ({ children }) => {
 	return (
 		<div>
-			<nav className={styles.nav}>Vocab Builder</nav>
+			<nav className={styles.nav}>
+				<Link href="/">
+					<a className={styles.header}>
+						<div className={styles.logo} />
+						<h2>Vocab Builder</h2>
+					</a>
+				</Link>
+			</nav>
 			{children}
 		</div>
 	);
