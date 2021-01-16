56e91dc6c0e01191df32abc2e84e568aa5de2d7f
sandeep chahal <Sandeepchahal573@gmail.com>
2021-01-16 10:00:43 +0530

    improved show component styling

--- components/showSelector/index.js
+++ components/showSelector/index.js
@@ -5,37 +5,17 @@ import styles from "./showSelector.module.css";
 
 const ShowSelector = ({ name, showId, cancel }) => {
 	const [totalSeasons, setTotalSeasons] = useState(null);
-	const [season, setSeason] = useState(100);
-	const [episode, setEpisode] = useState(100);
+	const [season, setSeason] = useState(1);
+	const [episode, setEpisode] = useState(1);
 	const router = useRouter();
 
 	useEffect(() => {
 		(async () => {
 			const seasons = await getShowInfo(showId);
-			if (seasons === null) return alert("something went wrong");
-			// if user selected wrong season and episode number already
-			if (!(season > 0) || !(season <= seasons.length)) {
-				setSeason(1);
-				setEpisode(1);
-			} else if (!(episode > 0) || !(episode <= seasons[season].length))
-				setEpisode(1);
 			setTotalSeasons(seasons);
 		})();
 	}, []);
 
-	const handleSeasonChange = (e) => {
-		const value = parseInt(e.target.value);
-		if (totalSeasons) {
-			if (value > 0 && value <= totalSeasons.length) setSeason(value);
-		} else setSeason(value);
-	};
-	const handleEpisodeChange = (e) => {
-		const value = parseInt(e.target.value);
-
-		if (totalSeasons) {
-			if (value > 0 && value <= totalSeasons[season - 1]) setEpisode(value);
-		} else setEpisode(value);
-	};
 	return (
 		<div className={styles.selectorWrapper}>
 			<div className={styles.selector}>
@@ -47,7 +27,7 @@ const ShowSelector = ({ name, showId, cancel }) => {
 					className={styles.input}
 					type="number"
 					value={season}
-					onChange={handleSeasonChange}
+					onChange={(e) => setSeason(e.target.value)}
 				/>
 				<div className={styles.label}>
 					<label>Episode</label>
@@ -57,7 +37,7 @@ const ShowSelector = ({ name, showId, cancel }) => {
 					className={styles.input}
 					type="number"
 					value={episode}
-					onChange={handleEpisodeChange}
+					onChange={(e) => setEpisode(e.target.value)}
 				/>
 				<div className={styles.buttonGrp}>
 					<button onClick={cancel} className={styles.button}>
